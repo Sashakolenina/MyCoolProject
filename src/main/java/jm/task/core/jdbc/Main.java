a8a1534bb64be808bb589100e944aadcac182e75
Sashakolenina <Sasha_kolenina@mail.ru>
2025-04-10 20:16:21 +0300

    Task1 JDBC Resolve conflicts

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,51 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        System.out.println("Таблица users создана");
+
+      userService.saveUser("Александра", "Гущина", (byte) 25);
+      System.out.println("Пользователь Александра добавлен");
+
+        userService.saveUser("Марина", "Маринина", (byte) 25);
+      System.out.println("Пользователь Марина добавлен");
+
+        List<User> users = userService.getAllUsers();
+        System.out.println("\nСписок всех пользователей:");
+        for (User user : users) {
+            System.out.println(user);
+        }
+
+        userService.removeUserById(2);
+        System.out.println("\nПользователь с id=2 удален");
+
+        users = userService.getAllUsers();
+        System.out.println("\nСписок пользователей после удаления:");
+        for (User user : users) {
+            System.out.println(user);
+        }
+
+        userService.cleanUsersTable();
+        System.out.println("\nТаблица users очищена");
+
+        users = userService.getAllUsers();
+        System.out.println("\nСписок пользователей после очистки:");
+        System.out.println("Количество пользователей: " + users.size());
+
+        userService.dropUsersTable();
+
     }
 }
+
+
+// реализуйте алгоритм здесь
+
--- src/main/java/jm/task/core/jdbc/dao/UserDao.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDao.java
@@ -17,3 +17,5 @@ public interface UserDao {
 
     void cleanUsersTable();
 }
+
+
